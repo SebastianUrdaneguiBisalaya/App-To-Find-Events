9e278e978f740df1b038b9cee08785fc4a780c5f
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-09-22 01:04:58 -0500

    add: Eslint and Prettier configuration.

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    endOfLine: "lf",
+    printWidth: 120,
+    arrowParens: "always",
+    singleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    ingleAttributePerLine: true,
+    htmlWhitespaceSensitivity: "css",
+};
+
+export default config;
