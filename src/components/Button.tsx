7bb1684c69e23ebaf95d61dd895b7d4bec170dd7
Alexa <alexamibco@gmail.com>
2024-09-25 16:21:55 -0500

    Fixing button measures

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-[10vw] py-[1.5vw] font-semibold text-white bg-[#761CBC] rounded-[10px] hover:bg-[#0DCDAA] font-poppins"
+      className="px-4 p-3 font-semibold text-white bg-[#761CBC] rounded-[10px] hover:bg-[#0DCDAA] font-poppins"
     >
       {text}
     </button>
