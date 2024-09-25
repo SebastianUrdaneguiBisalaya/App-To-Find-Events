f1129683f90ba121ab7857c91d6e0dc35f5704e2
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-09-25 09:35:49 -0500

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
-import { TotalAmountBar } from "./components/TotalAmountBar/TotalAmountBar";
-import { Texts } from "./components/TextTypes/Texts";
 
 function App() {
   return (
-    // <div className="text-3xl font-bold">
-    //   <h1>Hello World!</h1>
-    // </div>
-    // <TotalAmountBar totalAmount={1000}/>
-    <Texts
-      text="Hello World!"
-      type="title"
-      color="black"
-    />
+    <div className="text-3xl font-bold">
+      <h1>Hello World!</h1>
+    </div>
   );
 }
 
