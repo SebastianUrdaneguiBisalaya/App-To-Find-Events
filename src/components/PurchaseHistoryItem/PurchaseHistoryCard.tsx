b20f45ab50d2681da30af5a083d46a90774d6192
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-09-30 16:29:02 -0500

    add: PurchaseHistoryCard is already to use.

---
+++ src/components/PurchaseHistoryItem/PurchaseHistoryCard.tsx
@@ -0,0 +1,41 @@
+type PurchaseHistoryCardProps = {
+  dateBuy: string;
+  nameEvent: string;
+  priceEvent: number;
+  dateEvent: string;
+  placeEvent: string;
+  hourEvent: string;
+};
+
+export const PurchaseHistoryCard = ({
+  dateBuy,
+  nameEvent,
+  priceEvent,
+  dateEvent,
+  placeEvent,
+  hourEvent,
+}: PurchaseHistoryCardProps) => {
+  return (
+    <button className="flex flex-col md:w-[24rem] max-w[24rem] h-auto shadow-xl rounded-2xl p-4 font-poppins justify-start items-start">
+      <div className="w-full">
+        <p className="flex justify-between items-center text-left font-bold text-xs">
+          Día de compra: <span className="font-normal text-sm">{dateBuy ? dateBuy : ""}</span>
+        </p>
+        <div className="flex xs:flex-row flex-col justify-between xs:items-center items-start">
+          <h4 className="max-w-56 text-left text-base font-bold mt-2">{nameEvent ? nameEvent : ""}</h4>
+          <p className="text-base mt-2 font-bold text-green-800">{`S/. ${priceEvent ? priceEvent : ""}`}</p>
+        </div>
+        <p className="text-left text-xs text-gray-600 mt-2">Datos del evento:</p>
+        <p className="flex justify-between items-center text-left text-xs font-medium text-gray-500 mt-2">
+          Día: <span className="text-sm font-normal text-black">{dateEvent ? dateEvent : ""}</span>
+        </p>
+        <p className="flex justify-between items-center text-left text-xs font-medium text-gray-500">
+          Lugar: <span className="text-sm font-normal text-black">{placeEvent ? placeEvent : ""}</span>
+        </p>
+        <p className="flex justify-between items-center text-left text-xs font-medium text-gray-500">
+          Hora: <span className="text-sm font-normal text-black">{hourEvent ? hourEvent : ""}</span>
+        </p>
+      </div>
+    </button>
+  );
+};
