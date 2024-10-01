157b7d5fffa5df63d4d42ff2867bd1b8b177cafb
Alexa <alexamibco@gmail.com>
2024-10-01 15:26:05 -0500

    Integration with texts

--- src/components/MainDetailsCard.tsx
+++ src/components/MainDetailsCard.tsx
@@ -1,3 +1,5 @@
+import { Texts } from "./TextTypes/Texts";
+
 interface MainDetailsCardProps {
   artist: string;
   eventTitle: string;
@@ -8,20 +10,30 @@ interface MainDetailsCardProps {
 
 export const MainDetailsCard: React.FC<MainDetailsCardProps> = ({ artist, eventTitle, location, date, capacity }) => {
   return (
-    <div className="inline-block shadow-sm py-10 pl-6 pr-10 rounded-[14px]">
+    <div className=" bg-white mx-auto shadow-sm py-10 pl-6 pr-10 rounded-[14px] w-[346px] ">
       <div>
         <p className="font-semibold text-base">
           {artist} <span className="text-[#761CBC]">{eventTitle}</span>
         </p>
-        <p className="font-normal text-sm">
-          <span className="text-xs text-gray-500 mr-2 icon-[tdesign--location]"></span>
-          {location}
+        <p className="flex items-center">
+          <span className="text-xs text-[#2D264B50] mr-2 icon-[tdesign--location]"></span>
+          <Texts
+            text={location}
+            type="body"
+            color="black"
+            className="font-normal text-sm"
+          />
         </p>
-        <p className="font-normal text-sm">
-          <span className="text-xs text-gray-500 mr-2 icon-[solar--calendar-search-outline]"></span>
-          {date}
+        <p className="flex items-center">
+          <span className="text-xs text-[#2D264B50] mr-2 icon-[solar--calendar-search-outline]"></span>
+          <Texts
+            text={date}
+            type="body"
+            color="black"
+            className="font-normal text-sm"
+          />
         </p>
-        <p className="font- text-sm">{capacity}</p>
+        <p className="font-medium text-sm">{capacity}</p>
       </div>
     </div>
   );
