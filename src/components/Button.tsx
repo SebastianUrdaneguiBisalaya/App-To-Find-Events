e2dbb2d6e146886719e89f352fbdcaa33e991454
Alexa <alexamibco@gmail.com>
2024-10-01 16:44:42 -0500

    add classname prop for more responsiveness

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,11 @@ interface ButtonProps {
   className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ text, onClick, className = "" }) => {
   return (
     <button
       onClick={onClick}
-      className="px-4 p-3 font-semibold text-white bg-[#761CBC] rounded-[10px] hover:bg-[#0DCDAA] font-poppins"
+      className={`px-4 py-3 font-semibold text-white bg-[#761CBC] rounded-[10px] hover:bg-[#0DCDAA] font-poppins ${className}`}
     >
       {text}
     </button>
