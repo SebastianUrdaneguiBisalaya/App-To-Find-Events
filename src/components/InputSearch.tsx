314650d8da20a9e11c2fc734aff7c0899238e744
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-10-08 00:09:41 -0500

    fix: Remove the with hard of TrendingEventCard component and add the w-full in InputSearch component.

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -9,7 +9,7 @@ export const InputSearch = () => {
     console.log(startDate, endDate, searchQuery);
   };
   return (
-    <div className="max-w-3xl mx-auto p-4">
+    <div className="w-full max-w-3xl mx-auto">
       <form
         onSubmit={handleSearch}
         className="flex flex-col sm:flex-row items-center gap-2 p-2 bg-white sm:rounded-full rounded-2xl shadow-lg"
