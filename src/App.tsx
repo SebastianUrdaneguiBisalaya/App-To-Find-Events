9c892cd43723794b9af874903ce929eda2f43e26
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-10-07 23:41:34 -0500

    fix: Change the font-size of text in Input Search Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import { TrendingEvents } from "./views/TrendingEvents";
 function App() {
   return (
-    <div className="text-3xl font-bold">
-      <h1>Hello World!</h1>
-    </div>
+    // <div className="text-3xl font-bold">
+    //   <h1>Hello World!</h1>
+    // </div>
+    <TrendingEvents />
   );
 }
 
