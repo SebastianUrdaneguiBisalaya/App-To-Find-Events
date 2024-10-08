77da270f25516d6773a6af3e7c4a4e0230b51001
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-10-07 23:45:45 -0500

    fix: Remove the views folder because this not belong on this branch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import { TrendingEvents } from "./views/TrendingEvents";
+
 function App() {
   return (
-    // <div className="text-3xl font-bold">
-    //   <h1>Hello World!</h1>
-    // </div>
-    <TrendingEvents />
+    <div className="text-3xl font-bold">
+      <h1>Hello World!</h1>
+    </div>
   );
 }
 
