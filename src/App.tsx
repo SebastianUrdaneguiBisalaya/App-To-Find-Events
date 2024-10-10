be2eee205d82a14461fadd4c8169ca2c0f6264c2
Cabrakana <ana.patricia.ru@gmail.com>
2024-10-10 18:13:05 -0500

    fix: Added the following code to achieve a responsive component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import "./App.css";
 import { NavBar } from "./components/NavBar";
 function App() {
   return (
-    // <div className="text-3xl font-bold">
-    //   <h1>Hello World!</h1>
-    // </div>
-    <NavBar />
+    <div className="text-3xl font-bold">
+    <h1>Hello World!</h1>
+  </div>
   );
 }
 export default App;
